c47f26451a2932e09d689d06bba1e00afb57e4cd
Patrick Joyce <pjaudiomv@gmail.com>
2022-08-25 22:57:44 -0400

    add all keys

--- main.tf
+++ main.tf
@@ -8,11 +8,24 @@ data "http" "users" {
 }
 
 variable "github_users" {
-  type = list(string)
+  type        = list(string)
+  description = "A list of GitHub usernames."
+}
+
+variable "return_all_keys" {
+  type        = bool
+  default     = false
+  description = "By default we return only the users first ssh key, setting this to true returns all users keys."
+}
+
+locals {
+  first_keys = [for i in data.http.users : jsondecode(i.response_body).0.key]
+  all_keys   = [for i in flatten([for r in data.http.users : jsondecode(r.response_body)]) : i.key]
 }
 
 output "keys" {
-  value = [for i in data.http.users : jsondecode(i.response_body).0.key]
+  value       = var.return_all_keys ? local.all_keys : local.first_keys
+  description = "The list of all the keys."
 }
 
 terraform {
